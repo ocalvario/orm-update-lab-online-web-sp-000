f807fb56be33c1baa4dffa16d9c9fd5654ef10cc
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-10 20:22:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,15 @@ class Student
     @grade = grade
   end 
 
+
+  def self.new_from_db(row)
+     new_student = self.new  # self.new is the same as running Song.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student  
+  end
+  
    def self.create_table
     sql =  <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
