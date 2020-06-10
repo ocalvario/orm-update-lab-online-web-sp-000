eb1e8a6469c3a7f3844fdb0744b04264d93710f0
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-10 20:29:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :id, :name, :grade,
   
   def initialize (name, grade, id=nil)
     @id = id
