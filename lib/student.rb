c666166a489f71ca36da04019495dc5e2377e518
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-10 20:34:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
     self.new(id, name, grade)
   end 
   
-     def update
+   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.name)
   end
